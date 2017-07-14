e810f882a3c54b8d785a88363d5dfc8425920de4
James Kingsley <jameslkingsley@outlook.com>
2017-07-15 00:02:45 +0100

    Potential fix for #4

--- addons/ai/XEH_unitInit.sqf
+++ addons/ai/XEH_unitInit.sqf
@@ -8,12 +8,18 @@ if (isNull _unit || {!local _unit} || {isPlayer _unit}) exitWith {};
 
 [_unit] call FUNC(assignLoadout);
 
-if (leader _unit isEqualTo _unit) then {
-    private _group = group _unit;
-    private _garrisonEnabled = _group getVariable [QGVAR(GarrisonEnabled), false];
+[{
+    [{
+        params ["_unit"];
 
-    if (_garrisonEnabled) then {
-        private _radius = _group getVariable [QGVAR(GarrisonRadius), 250];
-        [_unit, nil, _unit, _radius] call FUNC(garrison);
-    };
-};
+        if (leader _unit isEqualTo _unit) then {
+            private _group = group _unit;
+            private _garrisonEnabled = _group getVariable [QGVAR(GarrisonEnabled), false];
+
+            if (_garrisonEnabled) then {
+                private _radius = _group getVariable [QGVAR(GarrisonRadius), 250];
+                [_unit, nil, _unit, _radius] call FUNC(garrison);
+            };
+        };
+    }, _this] call CBA_fnc_execNextFrame;
+}, [_unit]] call CBA_fnc_execNextFrame;
--- addons/ai/categories/garrison.hpp
+++ addons/ai/categories/garrison.hpp
@@ -8,7 +8,7 @@ class GVAR(Garrison) {
             tooltip = "Garrison all units in this group in nearby buildings.";
             property = QGVAR(GarrisonEnabled);
             control = "Checkbox";
-            expression = QUOTE(_this setVariable [ARR_3(QQGVAR(GarrisonEnabled), _value, true)]);
+            expression = QUOTE([ARR_2({(_this select 0) setVariable [ARR_3(QQGVAR(GarrisonEnabled), (_this select 1), true)]}, [ARR_2(_this, _value)])] call CBA_fnc_execNextFrame);
             defaultValue = 0;
         };
 
@@ -17,7 +17,7 @@ class GVAR(Garrison) {
             tooltip = "Radius in meters of the garrison range.";
             property = QGVAR(GarrisonRadius);
             control = "EditShort";
-            expression = QUOTE(_this setVariable [ARR_3(QQGVAR(GarrisonRadius), _value, true)]);
+            expression = QUOTE([ARR_2({(_this select 0) setVariable [ARR_3(QQGVAR(GarrisonRadius), (_this select 1), true)]}, [ARR_2(_this, _value)])] call CBA_fnc_execNextFrame);
             defaultValue = 250;
             validate = "number";
             typeName = "NUMBER";
