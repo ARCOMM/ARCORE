bbf8ad2e834b5fac3183ce46659c1560f4057a7f
James Kingsley <jameslkingsley@outlook.com>
2018-02-11 19:55:53 +0000

    Close #17

--- addons/main/CfgAmmo.hpp
+++ addons/main/CfgAmmo.hpp
@@ -3,17 +3,12 @@ class CfgAmmo {
     class FlareBase: FlareCore {};
 
     class Flare_82mm_AMOS_White: FlareCore {
-        intensity = 2500000;
+        intensity = 1250000;
     };
 
     class F_40mm_White: FlareBase {
         intensity = 1250000;
-        timeToLive = 300;
-        coefGravity = 1;
-    };
-
-    class rhs_40mm_white: F_40mm_White {
-        intensity = 1250000;
-        timeToLive = 300;
+        timeToLive = 40;
+        coefGravity = 0.25;
     };
 };
